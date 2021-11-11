b0df748be47e55e1bc365d5c2700d75d5c58a864
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-11-11 18:59:04 -0300

    feat: Feita a implementação das funções de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,81 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {
+        name: '',
+      },
+      isDisabled: true,
+      isLoading: false,
+      redirect: false,
+    };
+    this.onChangeInput = this.onChangeInput.bind(this);
+    this.handleRedirect = this.handleRedirect.bind(this);
+  }
+
+  async handleRedirect(event) {
+    event.preventDefault();
+    this.setState(({ isLoading: true }));
+    const { user } = this.state;
+    const response = await createUser(user);
+    if (response === 'OK') return this.setState({ redirect: true });
+  }
+
+  onChangeInput({ target }) {
+    const { value } = target;
+    const minLength = 3;
+    this.setState(({
+      user: {
+        name: value,
+      },
+      isDisabled: value.length < minLength,
+    }
+    ));
+  }
+
   render() {
+    const {
+      state: {
+        isDisabled,
+        isLoading,
+        redirect,
+      },
+      onChangeInput,
+      handleRedirect,
+    } = this;
+
     return (
-      <div data-testid="page-login"> </div>
+      <div data-testid="page-login">
+        <form onSubmit={ handleRedirect }>
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">Usuário</span>
+            <input
+              data-testid="login-name-input"
+              type="text"
+              className="form-control"
+              placeholder="Nome de Usuário"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+              onChange={ onChangeInput }
+            />
+            <button
+              data-testid="login-submit-button"
+              type="submit"
+              className="btn btn-primary btn-lg"
+              disabled={ isDisabled }
+            >
+              Entrar
+            </button>
+          </div>
+        </form>
+        { isLoading && <Loading /> }
+        { redirect && <Redirect to="/search" /> }
+      </div>
     );
   }
 }
