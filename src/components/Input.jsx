d51f58b4bcfb1d2a33c29a07371d294b8e07ac44
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-11-11 23:13:17 -0300

    feat: Feita a implementação de um component Input

---
+++ src/components/Input.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+
+export default class Input extends Component {
+  render() {
+    const { id, onChangeInput } = this.props;
+    return (
+      <input
+        data-testid={ id }
+        type="text"
+        className="form-control"
+        placeholder="Nome de Usuário"
+        aria-label="Username"
+        aria-describedby="basic-addon1"
+        onChange={ onChangeInput }
+      />
+    );
+  }
+}
+
+Input.propTypes = {
+  id: propTypes.string.isRequired,
+  onChangeInput: propTypes.func.isRequired,
+};
