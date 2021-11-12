633e302d6c4aeecdc04825b9725f30ca1a2dc432
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-11-11 23:15:08 -0300

    build: Feita a atualização das classes para estilização

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import './Loading.css';
 
-export default class Carregando extends Component {
+export default class Loading extends Component {
   render() {
     return (
-      <h1>
-        Carregando...
-      </h1>
+      <div className="loading">
+        <h1 className="loading-title">
+          Carregando...
+        </h1>
+      </div>
     );
   }
 }
