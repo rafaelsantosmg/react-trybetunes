8a7ced1505abffcfda83881145c754b9afb74508
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-11-14 21:47:55 -0300

    build: Feita a implementaçao do state para receber o array de musicas e passar para componente MusicCard

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 export default class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      album: [],
+      musics: [],
+    };
+    this.handleMusic = this.handleMusic.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleMusic();
+  }
+
+  async handleMusic() {
+    const { match } = this.props;
+    const response = await getMusics(match.params.id);
+    this.setState(({
+      album: response[0],
+      musics: response.filter((music, index) => index !== 0 && music),
+    }));
+  }
+
   render() {
+    const { musics, album } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        <h2 data-testid="artist-name">{ album.artistName }</h2>
+        <h3 data-testid="album-name">{ album.collectionName }</h3>
+        <img src={ album.artworkUrl100 } alt={ album.artistName } />
+        <MusicCard musics={ musics } />
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: propTypes.shape({
+    params: propTypes.shape({
+      id: propTypes.string,
+    }).isRequired,
+  }).isRequired,
+};
